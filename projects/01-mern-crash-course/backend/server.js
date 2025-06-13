efb85e2c7fe52a6fef23009b2c2d81bec94fd6da
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-06-13 23:20:01 +0200

    MERN crash course - Setting up the connection to the MongoDB;

--- projects/01-mern-crash-course/backend/server.js
+++ projects/01-mern-crash-course/backend/server.js
@@ -1,12 +1,17 @@
 // const exppress = require("express");
 import express from 'express'
+import dotenv from 'dotenv'
+import {connectDB} from './config/db.js'
+
+dotenv.config()
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('Server is ready')
-})
+app.get('/products', (request, response) => {})
+
+
 
 app.listen(5000, () => {
+  connectDB()
   console.log('Server started at http://localhost:5000')
 })
