96e58b9d4ca4e584983534e369f9fd2dc67ab978
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-06-13 22:53:59 +0200

    MERN crash course - Initial backend config;

---
+++ projects/01-mern-crash-course/backend/server.js
@@ -0,0 +1,12 @@
+// const exppress = require("express");
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Server is ready')
+})
+
+app.listen(5000, () => {
+  console.log('Server started at http://localhost:5000')
+})
