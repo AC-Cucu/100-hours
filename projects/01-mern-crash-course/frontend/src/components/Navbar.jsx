e891e66b687f5b4b412671b3363dd8a2f7d16b8e
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-06-15 19:28:00 +0200

    MERN - Frontend - Setting up the navbar component + Installing react-icons & chakra-ui-icons;

--- projects/01-mern-crash-course/frontend/src/components/Navbar.jsx
+++ projects/01-mern-crash-course/frontend/src/components/Navbar.jsx
@@ -1,8 +1,42 @@
+import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { PlusSquareIcon } from '@chakra-ui/icons'
+import { IoMoon} from 'react-icons/io5'
+import { LuSun } from 'react-icons/lu'
 
 const Navbar = () => {
+  const {colorMode, toggleColorMode} = useColorMode()
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={"16px"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", sm: "row" }}
+
+      >
+        <Text
+          bgGradient='linear(to-l,#6daaf0,#0f5ed3)'
+          bgClip='text'
+          fontSize='6xl'
+          fontWeight='extrabold'  
+        >
+          <Link to="/">Cucu's Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to="/create">
+            <Button>
+              <PlusSquareIcon fontSize={"20"} />
+            </Button>
+          </Link>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <IoMoon/> : <LuSun />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 
