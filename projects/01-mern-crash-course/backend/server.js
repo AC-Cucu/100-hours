f9ed785547a2b1cf4b20cf1dd8e67470e2169883
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-06-15 11:37:27 +0200

    MERN crash course - Creating products endpoint;

--- projects/01-mern-crash-course/backend/server.js
+++ projects/01-mern-crash-course/backend/server.js
@@ -1,14 +1,34 @@
 // const exppress = require("express");
 import express from 'express'
 import dotenv from 'dotenv'
-import {connectDB} from './config/db.js'
+import { connectDB } from './config/db.js'
+import Product from './models/product.model.js'
 
 dotenv.config()
 
 const app = express()
 
-app.get('/products', (request, response) => {})
-app.post('/products', (request, response) => {})
+// Middleware to parse JSON request bodies
+app.use(express.json())
+
+app.get('/api/products', (request, response) => {})
+app.post('/api/products', async (request, response) => {
+  const product = request.body // user will send product data in the request body
+  if (!product.name && !product.price && !product.image) {
+    response.status(400).json({ succes: false, message: 'Invalid product data, please provide all the fields' })
+  }
+
+  // create a new product instance
+  const newProduct = new Product(product)
+  try {
+    // save the product to the database
+    await newProduct.save()
+    response.status(201).json({ success: true, message: 'Product created successfully', data: product })
+  } catch (error) {
+    console.error('Error creating product:', error)
+    response.status(500).json({ success: false, message: 'Internal server error' })
+  }
+})
 
 app.listen(5000, () => {
   connectDB()
